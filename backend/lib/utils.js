92ada0279caa9d9addcfe6c227c6a701bd73ccc6
Neha-Singhal-0 <nehasinghal032415@gmail.com>
2025-08-05 20:28:37 +0530

    git first commit

---
+++ backend/lib/utils.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+     secure: process.env.NODE_ENV === "production",
+    // sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    // sameSite: "None",
+    // secure: true, // ✅ only true on Render
+    // secure: process.env.NODE_ENV !== "development",
+  });
+
+  return token;
+};
