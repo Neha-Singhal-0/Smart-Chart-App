c6e792ee8e28ed77654855530e452f4dc794676e
Neha-Singhal-0 <nehasinghal032415@gmail.com>
2025-08-08 18:41:00 +0530

    modified

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -8,13 +8,14 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-     sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-     secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    secure: process.env.NODE_ENV === "production",
     // sameSite: "strict", // CSRF attacks cross-site request forgery attacks
     // sameSite: "None",
-    // secure: true, // ✅ only true on Render
+    secure: true, // ✅ only true on Render
     // secure: process.env.NODE_ENV !== "development",
   });
 
   return token;
 };
+//
